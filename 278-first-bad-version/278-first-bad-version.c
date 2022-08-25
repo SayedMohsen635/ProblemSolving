05c8a1e61ac6caa07a932a8d31136f9d0a401a42
Sayed <sayedmohsen635@gmail.com>
2022-08-25 13:01:59 +0200

    Time: 0 ms (100.00%), Space: 5.4 MB (66.06%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.c
+++ 278-first-bad-version/278-first-bad-version.c
@@ -2,11 +2,9 @@
 // bool isBadVersion(int version);
 
 int firstBadVersion(int n) {
-    int bad = 0;
-    int left = 1;
-    int right = n;
+    int mid , bad = 0 , left = 1 , right = n;
     while(left <= right){
-        int mid = left + ((right - left) / 2);
+        mid = left + (right - left) / 2;
         if(isBadVersion(mid)){
             bad = mid;
             right = mid - 1;
