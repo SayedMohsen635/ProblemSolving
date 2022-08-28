d79b6f03cf577b97ac876088151e01e2b6cf1e80
Sayed <sayedmohsen635@gmail.com>
2022-08-28 11:54:17 +0200

    Time: 9 ms (72.34%), Space: 7.1 MB (42.55%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.c
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.c
@@ -1,24 +1,11 @@
 char * reverseWords(char * s){
-    // char start = 0 , end = 1;
-    // while(1){
-    //     if(s[end] == '\0'){
-    //         reverseString(s + start , end - start);
-    //         return s;
-    //     }else if(s[end] == ' '){
-    //         reverseString(s + start , end - start);
-    //         start = end + 1;
-    //     }
-    //     end++;
-    // }
-    int start = 0;
-    int end = 1;
-    while (true) {
-        if (s[end] == '\0') {
-            reverseString(s + start, end - start);
+    int start = 0 , end = 1;
+    while(1){
+        if(s[end] == '\0'){
+            reverseString(s + start , end - start);
             break;
-        }
-        else if (s[end] == ' ') {
-            reverseString(s + start, end - start);
+        }else if(s[end] == ' '){
+            reverseString(s + start , end - start);
             start = end + 1;
         }
         ++end;
