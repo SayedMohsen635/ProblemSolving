9bbfbab4f34b6232ebc78318f000277a3f8ee564
Sayed <sayedmohsen635@gmail.com>
2022-08-28 10:11:43 +0200

    Time: 62 ms (79.84%), Space: 12.3 MB (56.14%) - LeetHub

---
+++ 344-reverse-string/344-reverse-string.c
@@ -0,0 +1,14 @@
+void reverseString(char* s, int sSize){
+    int temp;
+    for(int i = 0 , j = sSize - 1; i < sSize / 2; i++ , j--){
+        temp = s[i];
+        s[i] = s[j];
+        s[j] = temp;
+        if(i == j){
+            return;
+        }
+        if(sSize % 2 != 0 && j < i){
+            return;
+        }
+    }
+}
